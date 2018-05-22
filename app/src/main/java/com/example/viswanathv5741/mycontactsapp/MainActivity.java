1ef0b031adb50c6c4b8a565fa49d97185b902c7e
Vainavi Viswanath <ViswanathV5741@sduhsd.lan>
2018-05-22 08:17:16 -0700

    started my contact

---
+++ app/src/main/java/com/example/viswanathv5741/mycontactsapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.viswanathv5741.mycontactsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
