ef4cef713a860c1e5b2f4c6c65ac3b0ad3664c67
Vainavi Viswanath <ViswanathV5741@sduhsd.lan>
2018-05-22 09:00:24 -0700

    created databasehelper

--- app/src/main/java/com/example/viswanathv5741/mycontactsapp/MainActivity.java
+++ app/src/main/java/com/example/viswanathv5741/mycontactsapp/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.viswanathv5741.mycontactsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper myDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myDb = new DatabaseHelper(this);
+        Log.d("MyContactApp", "MainActivity: instantiated DatabaseHelper");
+
     }
 }
